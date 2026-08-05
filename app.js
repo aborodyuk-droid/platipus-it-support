c54f50a4b2556a6767a69fdc22ca69469f88e47b
Oleksandr Borodiuk <work@Oleksandrs-MacBook-Pro.local>
2026-08-05 09:37:08 +0300

    Connect Create IT Request

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const button = document.getElementById("createTicket");
+document.getElementById("createTicket").onclick = function () {
 
-button.addEventListener("click", () => {
-    alert("The button works!");
-});
+    window.open(
+        "https://platipus.myjetbrains.com/youtrack/form/cc5097c4-529b-4903-be60-1ae1e8060f07",
+        "_blank"
+    );
+
+};
+
+document.getElementById("knowledgeBase").onclick = function () {
+
+    alert("Knowledge Base will be available soon.");
+
+};
+
+document.getElementById("contactIT").onclick = function () {
+
+    alert("Contact IT feature will be available soon.");
+
+};
