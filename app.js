9938ff4236c4d162c55a1233ba08d6a4d9bc3c01
Oleksandr Borodiuk <work@Oleksandrs-MacBook-Pro.local>
2026-08-05 11:30:14 +0300

    Create IT Request opens YouTrack

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-document.getElementById("createRequest").addEventListener("click", () => {
+// ===============================
+// Create IT Request
+// ===============================
+
+document.getElementById("createTicket").addEventListener("click", () => {
+
     window.open(
         "https://platipus.myjetbrains.com/youtrack/form/cc5097c4-529b-4903-be60-1ae1e8060f07",
         "_blank"
     );
+
 });
+
+
+// ===============================
+// Knowledge Base
+// ===============================
 
 document.getElementById("knowledgeBase").addEventListener("click", () => {
-    alert("Knowledge Base");
+
+    alert("Knowledge Base will be available soon.");
+
 });
 
+
+// ===============================
+// Contact IT
+// ===============================
+
 document.getElementById("contactIT").addEventListener("click", () => {
-    alert("Contact IT");
+
+    alert("Contact IT will be available soon.");
+
 });
